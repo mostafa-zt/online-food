a5749f2eabf40e9a3afea8456db6a35b1c92660f
mostafa <you@example.com>
2021-02-22 00:07:40 -0500

    first commit

--- OnlineFood.App/wwwroot/lib/cedarmap/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    HTTP_URL: window.apiBaseUrlHttp || 'http://api.cedarmaps.com/v1',
-    HTTPS_URL: window.apiBaseUrlHttps || 'https://api.cedarmaps.com/v1',
-    FORCE_HTTPS: true,
-    REQUIRE_ACCESS_TOKEN: true
-};
